7a1df79c1f516f98a8ea034e8ad54f8bec3b60b3
Evgeny Ivanov <es.ivanov@zenit.ru>
2025-10-02 13:12:05 +0300

    HW8 env_reader realized

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,54 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make(Environment)
+
+	for _, entry := range entries {
+		name := entry.Name()
+		// файлы с названием, содержащим "=" игнорируем
+		if strings.Contains(name, "=") {
+			continue
+		}
+		path := filepath.Join(dir, name)
+
+		ev, err := makeFromFile(path)
+		if err == nil {
+			ret[name] = ev
+		}
+	}
+
+	return ret, nil
+}
+
+func makeFromFile(path string) (EnvValue, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return EnvValue{}, err
+	}
+	defer func() { _ = f.Close() }()
+
+	var fLine string
+	var val EnvValue = EnvValue{NeedRemove: true}
+
+	scanner := bufio.NewScanner(f)
+	if scanner.Scan() {
+		fLine = scanner.Text()
+		if len(fLine) > 0 {
+			val.NeedRemove = false
+		}
+		fLine = strings.ReplaceAll(fLine, "\x00", "\n")
+		fLine = strings.TrimRight(fLine, " \t")
+	}
+	if errS := scanner.Err(); errS != nil {
+		return EnvValue{}, errS
+	}
+
+	val.Value = fLine
+
+	return val, nil
 }
